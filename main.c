5bac6426bd6737385a64673e9f6f38283cf30735
dkalll <dkalwkdwnsdud@gmail.com>
2025-10-24 00:33:56 +0900

    main.c

--- main.c
+++ main.c
@@ -2,11 +2,33 @@
 
 #define SIZE 5
 
+void inputArray(int arr[]){
+ printf("정수 5개를 입력하세요:\n");
+ for(int i=0;i<SIZE; i++){
+  printf("%d번째 숫자:",i+1);
+  scanf_s("%d",&arr[i]);
+  }
+}
+
 int main(void){
+ int numbers[SIZE];
+ int maxResult,minResult;
+ int sum;
+ float average;
 
+ inputArray(numbers);
 
+ maxResult=findMax(numbers);
+
+ minResult=findMin(numbers);
+
+ sum=findSum(numbers);
+ average=(float)sum/SIZE;
+
+printfResults(numbers,maxResult,minResult,sum,average);
+ 
   return 0;
-};
+}
 
 int findSum(int arr[]) {
     int sum = 0;
