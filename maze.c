a15330cfd2121e4792a3435333d052c52e1025a9
kth663 <stest12345s@gmail.com>
2025-11-12 21:32:58 +0900

    feat: read maze.txt

--- maze.c
+++ maze.c
@@ -9,8 +9,14 @@ int maze(){
 
     char map[27][27];
 
+    for(int i = 0;i<27;i++){
+        for(int j = 0;j<27;j++){
+            fscanf_s(mazeFile, "%c", &map[i][j], 1);
+        }
+        // 엔터를 저장하지 않기 위해 엔터 읽기
+        char temp;
+        fscanf_s(mazeFile,"%c", &temp, 1); 
+    }
+
     
-
-
-
 }
