6f7f4e5b33a4d73c98ca208c9cbbb655f9493212
kth663 <stest12345s@gmail.com>
2025-11-12 22:22:09 +0900

    feat: add player and position

--- maze.c
+++ maze.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<windows.h>
+#include<locale.h>
+
+void setCursorPos(int x,int y);
 
 int maze(){
     
+    // ■와 ●가 ??로 출력되는 문제 때문에 설정
+    setlocale(LC_CTYPE, "ko_KR.UTF-8");
+    //명령어 실행후 메시지가 나오지 않게 함
+    system("chcp 65001 > nul");
+
     FILE* mazeFile;
     fopen_s(&mazeFile, "maze.txt", "r");
 
@@ -34,4 +42,21 @@ int maze(){
         printf("\n");
     }
 
+    int x = 1;
+    int y = 1;
+
+    setCursorPos(x, y);
+
+    printf("● ");
+    
+
+}
+
+void setCursorPos(int x,int y){
+    COORD pos;
+    //문자열의 가로가 2칸이기 때문에 2를 곱함
+    pos.X = x * 2;
+    pos.Y = y;
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
