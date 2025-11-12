da1e7c99c1d85fb0e8868ccdde6cb91f4350d6d4
kth663 <stest12345s@gmail.com>
2025-11-12 21:24:38 +0900

    feat: open mazeFile.txt

---
+++ maze.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+int maze(){
+    
+    FILE* mazeFile;
+    fopen_s(&mazeFile, "maze.txt", "r");
+
+    char map[27][27];
+
+    
+
+
+
+}
