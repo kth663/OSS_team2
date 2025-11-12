201b5e8e7dfde0842dfe5a01e55fc27acefa8784
kth663 <stest12345s@gmail.com>
2025-11-12 22:27:08 +0900

    feat: close mazeFile

--- maze.c
+++ maze.c
@@ -26,6 +26,8 @@ int maze(){
         fscanf_s(mazeFile,"%c", &temp, 1); 
     }
 
+    fclose(mazeFile);
+
     for(int i = 0;i<27;i++){
         for(int j= 0;j<27;j++){
             char ch = map[i][j];
