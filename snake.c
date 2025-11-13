a28f97a3c617a8e7ca4ccb59ec83c66efc0fc706
kth663 <stest12345s@gmail.com>
2025-11-13 15:31:51 +0900

    fix: resolve broken charactor for consol output

--- snake.c
+++ snake.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<locale.h>
 
 void snackGame(){
 
-
+    // ■와 ●가 ??로 출력되는 문제 때문에 설정
+    setlocale(LC_CTYPE, "ko_KR.UTF-8");
+    //명령어 실행후 메시지가 나오지 않게 함
+    system("chcp 65001 > nul");
 
     int mapSize = 5;
     mapSize += 2;
