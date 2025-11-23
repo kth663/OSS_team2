fa99612593e8887cf2a52cfb61fac17a049d61cb
장준영 / JANG JUN YEONG <dkalwkdwnsdud@gmail.com>
2025-11-23 20:52:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
+// slot_and_game.c 안에 있는 함수 선언
+void runSlotMachine(void);
 
+int main(void) {
+    // 슬롯머신 게임 시작
+    runSlotMachine();
 
-    
+    return 0;
 }
