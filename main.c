f63ae8c2e08346475e74e006efe5f6d06cc199e7
장준영 / JANG JUN YEONG <dkalwkdwnsdud@gmail.com>
2025-11-23 20:40:43 +0900

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(void){
 
+srand((unsigned int)time(NULL));
+
+    // 게임 시작
+    runGameLoop();
+
+    return 0;
 
     
 }
