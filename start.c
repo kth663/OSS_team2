bc6108912c09d1807a608248fd478cea2f90c137
DESKTOP-HIJNJCS\user <lili10148-@chungbuk.ac.kr>
2025-12-03 21:10:50 +0900

    Change The Logo UI

--- start.c
+++ start.c
@@ -5,64 +5,81 @@
 #include "start.h"
 #include "maze.h"
 #include "data.h"
-#include "data.h"
 
 void gotoxy2(int x, int y) {
     COORD pos = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
+// --------------------------
+//       THE LOOP 로고
+//   (위에서 천천히 내려오기)
+// --------------------------
+void print_logo() {
+    const char* logo[] = {
+        "  █████████  ███   ███  ███████          ███        █████      █████    ██████",
+        "     ███     ███   ███  ███              ███      ███   ███  ███   ███  ███  ███",
+        "     ███     █████████  ██████           ███      ███   ███  ███   ███  ██████",
+        "     ███     ███   ███  ███              ███      ███   ███  ███   ███  ███",
+        "     ███     ███   ███  ███████          ████████   █████      █████    ███"
+    };
+
+    int lines = 5;
+    int finalY = 3;   // 로고가 멈출 최종 Y 위치
+    int startY = -5;  // 화면 위 밖에서부터 떨어지는 느낌
+
+    for (int y = startY; y <= finalY; y++) {
+        system("cls");  // 이전 프레임 지우기
+
+        // 현재 y 위치에 로고 출력
+        for (int i = 0; i < lines; i++) {
+            int drawY = y + i;
+            if (drawY >= 0) {  // 화면 위를 벗어나는 줄은 출력 안 함
+                gotoxy2(2, drawY); // 왼쪽에서 약간 들여쓰기
+                printf("%s", logo[i]);
+            }
+        }
+
+        Sleep(80);  // 떨어지는 속도 (원하면 조절 가능)
+    }
+}
+// ---------------------------------
+
+
 void show_loading_bar() {
     int barLength = 30;
 
     for (int i = 0; i <= barLength; i++) {
-        gotoxy2((SCREEN_WIDTH - (barLength + 10)) / 2, SCREEN_HEIGHT / 2 + 2);
+        gotoxy2((SCREEN_WIDTH - (barLength + 10)) / 2, SCREEN_HEIGHT / 2 + 7);
 
         printf("[");
         for (int j = 0; j < i; j++) printf("■");
         for (int j = i; j < barLength; j++) printf("□");
         printf("] %d%%", (i * 100) / barLength);
 
-        Sleep(100);
+        Sleep(50);
     }
 }
 
 void blink_press_enter() {
-char *msg = "Press ENTER to Start";
-  int msgX = (SCREEN_WIDTH - strlen(msg)) / 2;
+    char *msg = "Press ENTER to Start";
+    int msgX = (SCREEN_WIDTH - strlen(msg)) / 2;
 
-  while (1) {
-      gotoxy2(msgX, SCREEN_HEIGHT / 2 + 5);
-      printf("%s", msg);
-      Sleep(500);
+    while (1) {
+        gotoxy2(msgX, SCREEN_HEIGHT / 2 + 10);
+        printf("%s", msg);
+        Sleep(500);
 
-      gotoxy2(msgX, SCREEN_HEIGHT / 2 + 5);
-      printf("%*s", (int)strlen(msg), "");  
-      Sleep(300);
-        if (kbhit()) {              
+        gotoxy2(msgX, SCREEN_HEIGHT / 2 + 10);
+        printf("%*s", (int)strlen(msg), "");
+        Sleep(300);
+
+        if (kbhit()) {
             int key = getch();
-
-            
-            if (key == 27) {
-                return;
-            }
-
-            
+            if (key == 27) return; 
             if (key == 13) {
-
                 system("cls");
-                printf("Loading");
-                fflush(stdout);
-
-                for (int i = 0; i < 5; i++) {
-                    Sleep(300);
-                    printf(".");
-                    fflush(stdout);
-                }
-
-                Sleep(500);
-                system("cls");
-                break;
+                return;
             }
         }
     }
@@ -70,21 +87,15 @@ char *msg = "Press ENTER to Start";
 
 void start_screen() {
     system("cls");
-    system("color 0E");
+    system("color 0E");   // 노란색 유지
 
-    char *title = "★ The Loop★";
-    int titleX = (SCREEN_WIDTH - strlen(title)) / 2;
-    int titleY = SCREEN_HEIGHT / 2 - 3;
-
-    gotoxy2(titleX, titleY);
-    printf("%s", title);
-
-    show_loading_bar();
+    print_logo();         // ← 이제 떨어지는 애니메이션
+    show_loading_bar();   // 로고 아래에 그대로 이어서 나옴
     blink_press_enter();
 
-    system("color 07");
+    system("color 07");   // 원래 색으로 복귀
 
     x = 1;
-    y = 1; 
+    y = 1;
     maze();
 }
