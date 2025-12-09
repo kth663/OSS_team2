a854e4666c9bea654b53f92c3c9ebb5f519cbc1a
dkalll <dkalwkdwnsdud@gmail.com>
2025-12-09 18:51:02 +0900

    인벤토리 단서 보기 기능 추가

--- slot and game.c
+++ slot and game.c
@@ -16,7 +16,21 @@ void openInventory(void) {
     printf("===== 인벤토리 =====\n");
     printf("점수: %d\n", score);
     printf("행운의 부적: %d 개\n", getLuckyCharmCount);
+
+    printf("\n===== 비밀번호 단서 =====\n");
+
+    // password = 실제 정답 (예: abcde)
+    // getPassword[i] = 1이면 단서 구매한 것
+    for (int i = 0; i < 5; i++) {
+        if (getPassword[i] == 1) {
+            printf("%d번 글자: %c\n", i + 1, password[i]);
+        } else {
+            printf("%d번 글자: ?\n", i + 1);
+        }
+    }
+
     printf("\n아무 키나 누르면 돌아갑니다.\n");
     _getch();
     maze(0);
 }
+
