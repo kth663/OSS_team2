df8e725993abafe95a7e2a263992edc8117fe9ce
kth663 <stest12345s@gmail.com>
2025-12-09 14:55:31 +0900

    fix: change maze print for error

--- maze.c
+++ maze.c
@@ -149,12 +149,6 @@ void maze(int do_Animation){
                 else if(j != 0 && map[i][j-1] == 'X' && i != 0 && map[i-1][j] == 'X' && j != 26 && map[i][j+1] == 'X'){
                     printf("┻━");
                 }
-                else if(i != 0 && map[i-1][j] == 'X' && i != 26 && map[i+1][j] == 'X'){
-                    printf("┃ ");
-                }
-                else if(j != 0 && map[i][j-1] == 'X' && j != 26 && map[i][j+1] == 'X'){
-                    printf("━━");
-                }
                 else if(i != 26 && map[i+1][j] == 'X' && j != 26 && map[i][j+1] == 'X'){
                     printf("┏━");
                 }
@@ -166,6 +160,21 @@ void maze(int do_Animation){
                 }
                 else if(i != 26 && map[i+1][j] == 'X' && j != 0 && map[i][j-1] == 'X'){
                     printf("┓ ");
+                }
+                else if(i != 0 && map[i-1][j] == 'X' && i != 26 && map[i+1][j] == 'X'){
+                    printf("┃ ");
+                }
+                else if(j != 0 && map[i][j-1] == 'X' && j != 26 && map[i][j+1] == 'X'){
+                    printf("━━");
+                }
+                else if(i != 0 && map[i-1][j] == 'X' || i != 26 && map[i+1][j] == 'X'){
+                    printf("┃ ");
+                }
+                else if(j != 0 && map[i][j-1] == 'X'){
+                    printf("━ ");
+                }
+                else if(j != 26 && map[i][j+1] == 'X'){
+                    printf(" ━");
                 }
                 else{
                     printf("  ");
